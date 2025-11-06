18411438ccabbfe40dcc809ad8399cfda3d906e6
Samuel Marcos <samuel.marcos.smh@gmail.com>
2025-11-06 04:24:37 -0300

    style: removing emoji

--- run-exit-java.bat
+++ run-exit-java.bat
@@ -1,7 +1,7 @@
 mvn spring-boot:run | Tee-Object -Variable output | ForEach-Object {
     $_
     if ($_ -match "Started .*Application") {
-        Write-Host "✅ Aplicação iniciada com sucesso. Encerrando processo..."
+        Write-Host "Aplicação iniciada com sucesso. Encerrando processo..."
         Stop-Process -Id $PID
     }
 }
